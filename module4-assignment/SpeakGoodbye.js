0b63afd7afec2a63c01c66ae0c40cef48c103213
unknown <likhipriya2001@gmail.com>
2020-08-29 21:12:29 +0530

    module4-solution

---
+++ module4-assignment/SpeakGoodbye.js
@@ -0,0 +1,12 @@
+
+
+var speakWord = "Good Bye";
+  
+  (function(window){
+  	   var byeSpeaker={};
+  	   byeSpeaker.speakWord =speakWord;
+  	   byeSpeaker.speak=function(name) {
+        console.log(byeSpeaker.speakWord + " " + name);
+        }
+       window.byeSpeaker=byeSpeaker;
+   })(window);
